7e2ec1e9585116f71f26bdb445f361de4e75f5b7
Nithin S <nithinselvam333@gmail.com>
2025-08-07 22:55:57 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Transaction from "./pages/Transaction";
+import { AuthProvider } from "./context/AuthContext";
+
+function App() {
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/transaction" element={<Transaction />} />
+      </Routes>
+    </AuthProvider>
+  );
+}
+
+export default App;
